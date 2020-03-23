06d03c69820e8a85e90a536993eb85b5cf8b2cf0
kappyhappy <t.katsu1031@gmail.com>
2020-03-23 16:16:00 +0900

    update setup.sh for tfenv, etc

--- setup.sh
+++ setup.sh
@@ -8,18 +8,42 @@ brew install git vim neovim
 
 # vim setup
 mkdir -p $HOME/.vim/{colors,undo}
-git clone https://raw.githubusercontent.com/kappyhappy/setup/master/.vimrc
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh) ~/.cache/dein"
+curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+chmod 755 installer.sh
+./installer.sh ~/.cache/dein
+rm installer.sh
 
-git clone https://raw.githubusercontent.com/kappyhappy/setup/master/.vimrc
-git clone https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim $HOME/.vim/colors/
+git clone https://raw.githubusercontent.com/kappyhappy/setup/master/.vimrc $HOME/.vimrc
+git clone https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim $HOME/.vim/colors/hybrid.vim
 
 # zsh setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/
-git clone https://github.com/zsh-users/zsh-completions.git $HOME/.oh-my-zsh/plugins/
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions.git $HOME/.oh-my-zsh/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
 
-git clone https://raw.githubusercontent.com/kappyhappy/setup/master/.zshrc
+git clone https://raw.githubusercontent.com/kappyhappy/setup/master/.zshrc $HOME/.zshrc
+
+# pr jumper
+git clone https://github.com/kappyhappy/open-pull-request-URL.git 
+cp open-pull-request-URL/pr /usr/local/bin/pr
+rm -rf open-pull-request-URL
+
+# install tfenv
+git clone https://github.com/tfutils/tfenv.git $HOME/.tfenv
+
+# install goenv
+git clone https://github.com/syndbg/goenv.git $HOME/.goenv
+mkdir $HOME/go
+
+# install pyenv
+git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+
+# install rbenv
+brew install rbenv ruby-build
+
+# instal kubens
+brew install kubectx
+
 source $HOME/.zshrc
